Fix: gradient exploding in (pretrain mfrf by vc feat target) 3

downstream/UNITER/pretrain_vcr.py
@@ -260,13 +260,15 @@ def main(opts):
     start = time()
     # quick hack for amp delay_unscale bug
     optimizer.zero_grad()
-    optimizer.step()
+    optimizer.step();
     for step, (name, batch) in enumerate(meta_loader):
         # forward pass
         n_examples[name] += batch['input_ids'].size(0)
         n_in_units[name] += (batch['attn_masks'] == 1).sum().item()
         task = name.split('_')[0]
         loss = model(batch, task=task, compute_loss=True)
+        #if torch.isinf(loss).any():
+        #    import ipdb;ipdb.set_trace(context=10)
         n_loss_units[name] += loss.size(0)
         loss = loss.mean()  # loss is not normalized in model
 
@@ -402,12 +404,14 @@ def validate_mrfr(model, val_loader):
     LOGGER.info("start running MRFR validation...")
     val_loss = 0
     n_feat = 0
-    st = time()
+    st = time();#import ipdb;ipdb.set_trace(context=10)
     for i, batch in enumerate(val_loader):
         loss = model(batch, task='mrfr', compute_loss=True)
+        #if torch.isinf(loss).any():
+        #    import ipdb;ipdb.set_trace(context=10)
         val_loss += loss.sum().item() / IMG_DIM
         n_feat += batch['img_mask_tgt'].sum().item()
-    val_loss = sum(all_gather_list(val_loss))
+    val_loss = sum(all_gather_list(val_loss));#import ipdb;ipdb.set_trace(context=10)
     n_feat = sum(all_gather_list(n_feat))
     tot_time = time()-st
     val_loss /= n_feat